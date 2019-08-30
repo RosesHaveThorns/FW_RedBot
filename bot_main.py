1a86f58cddc916ed2574e9cdad25441a135f1103
cowminer27 <20016090+cowminer27@users.noreply.github.com>
2019-08-30 14:38:25 +0100

    Began setting up main script
    unfinished

--- bot_main.py
+++ bot_main.py
@@ -1,1 +1,10 @@
+import discord
+from discord.ext import commands
 
+TOKEN = '__PUT DISCORD BOT TOKEN HERE__'
+
+client = commands.Bot(command_prefix = '$')
+
+@client.event
+async def on_ready():
+	
