f6dce2723f0f98eebc9ae6d63f788a1b63cd211d
cowminer27 <mbrindle99@yahoo.com>
2020-04-30 15:14:06 +0100

    Update Moderation.py

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -20,31 +20,31 @@ class Moderation(commands.Cog):
 	@commands.command()
 	async def report(self, context):
     
-    self.logs.log("'$report' command called")    
-    
-    dateObj = datetime.datetime.now()
-    dateStr = dateObj.strftime("%a %d %b - %H:%M %Z")
-    
-    msg1 = context.message.author.name + " (" + context.message.author.nick + ")"
-    msg2 = context.message.content.split(" ")[1].strip()
-    msg3temp = context.message.content.split(" ")
-    
-    await context.message.delete()
-    
-    msg3 = ""
-    for i in range(1, len(msg3temp)):
-      msg3 = msg3 + " " + msg3temp[i]
-    
-    reportChnl = client.get_channel(self.reportChannelID)
-    
-    embed = discord.Embed(title="**REPORT**: " + dateStr, color=0x4287f5)
-    embed.add_field(name="User Reported By: ", value=msg1, inline=False)
-    embed.add_field(name="User Reported: ", value=msg2, inline=False)
-    embed.add_field(name="Reason: ", value=msg3, inline=False)
-    
-    await reportChnl.send(embed=embed)
-		
-		self.logs.log("Command Succesfull")
+	    self.logs.log("'$report' command called")    
+
+	    dateObj = datetime.datetime.now()
+	    dateStr = dateObj.strftime("%a %d %b - %H:%M %Z")
+
+	    msg1 = context.message.author.name + " (" + context.message.author.nick + ")"
+	    msg2 = context.message.content.split(" ")[1].strip()
+	    msg3temp = context.message.content.split(" ")
+
+	    await context.message.delete()
+
+	    msg3 = ""
+	    for i in range(1, len(msg3temp)):
+	      msg3 = msg3 + " " + msg3temp[i]
+
+	    reportChnl = client.get_channel(self.reportChannelID)
+
+	    embed = discord.Embed(title="**REPORT**: " + dateStr, color=0x4287f5)
+	    embed.add_field(name="User Reported By: ", value=msg1, inline=False)
+	    embed.add_field(name="User Reported: ", value=msg2, inline=False)
+	    embed.add_field(name="Reason: ", value=msg3, inline=False)
+
+	    await reportChnl.send(embed=embed)
+
+			self.logs.log("Command Succesfull")
 		
 def setup(client):
 	client.add_cog(Moderation(client))
